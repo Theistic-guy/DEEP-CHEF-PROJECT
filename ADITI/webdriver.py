7c5f6f5ef57a0dd5c4860a7a53f1fb7d0cdb1358
aditi-singh-21 <aditisinghrk906@gmail.com>
2024-04-22 21:17:00 +0530

    ddd

--- ADITI/webdriver.py
+++ ADITI/webdriver.py
@@ -11,40 +11,41 @@ import pandas as pd
 
 path = "C:\\Users\\aditi\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
 driver=webdriver.ChromeService(executable_path=path) #creates an instance of Chrome WebDriver, which allows you to interact  with the Chrome browser.
-df = pd.read_csv("recipes.csv",skiprows= 0,nrows=10,names=['name','link'])
-df.head()
 try:
     driver = webdriver.Chrome(service=driver) # Create a new Chrome session and pass the service object
     driver.maximize_window()
     driver.get("https://images.google.com/")
-    time.sleep(3)
+    time.sleep(10)
     search_box=driver.find_element(By.NAME,'q')
     
     start=1
     count=1
     maximum_images=10
-    df = pd.read_csv("recipes.csv",skiprows= start,nrows=10,names=['name','link'])
+    df = pd.read_csv('C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\ADITI\\recipes.csv',skiprows= start,nrows=count,names=['name','link'])
     
     for i in range(len(df['name'])):
         recipe_name=df.loc[i,'name']
-        print(start+i-1,recipe_name)
+        print(start+i,recipe_name)
         
         search_box.send_keys(recipe_name)
-        search_box.send_keys(Keys.RETURN)
+        search_box.send_keys(Keys.ENTER)
         time.sleep(7)
         
-        image_urls=set()
+        
         thumbnails=driver.find_elements(By.CLASS_NAME,'mNsIhb')
         
-        while(len(thumbnails) < maximum_images+5):
+        while(len(thumbnails) < 15):
+            print("less_images")
             time.sleep(3)
             driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
             time.sleep(3)
             thumbnails = driver.find_elements(By.CLASS_NAME,"mNsIhb") 
-            
+        
+        image_urls=set()  
+        count=0
         for thumbnail in thumbnails:
-            
-            if(len(image_urls) == maximum_images):
+            print(len(image_urls))
+            if(count == maximum_images):
                 break
             try:
                 thumbnail.click()
@@ -55,13 +56,23 @@ try:
             
             pop_up=driver.find_elements(By.CLASS_NAME,'jlTjKd')
             for elem in pop_up:
-                images=elem.find_elements(By.TAG_NAME,"img")
-                for image in images:
-                    if(image.get_attribute("class") == "sFlh5c pT0Scc iPVvYb" and image.get_attribute("src") not in image_urls):
-                        url=image.get_attribute("src")
-                        image_urls.add(url)
-            time.sleep(7)
-    time.sleep(7)
+                if(elem.tag_name == 'a'):
+                  images=elem.find_elements(By.TAG_NAME,"img")
+                  print(len(images))
+                  for image in images:
+                    print(image.get_attribute("class") )
+                    if len(image.get_attribute("class")) >=3 :
+                            third = image.get_attribute("class").split()[2]
+                            if third == "iPVvYb":
+                                img_url = image.get_attribute("src")
+                                if img_url not in image_urls :
+                                    image_urls.add(img_url)
+                                    count=count+1
+                            else:
+                                continue
+
+
+        time.sleep(7)
 except:
     print("unsuccess")      
             
