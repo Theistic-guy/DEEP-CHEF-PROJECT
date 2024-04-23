db242b13a76a5f6ad107abe485a57ed8b79ba3cf
aditi-singh-21 <aditisinghrk906@gmail.com>
2024-04-23 11:49:29 +0530

    new

--- ADITI/webdriver.py
+++ ADITI/webdriver.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import csv
 import pandas as pd
+import os
 
 
 def download_image(url,folder,img_no,recipe_name):
@@ -15,18 +16,18 @@ def download_image(url,folder,img_no,recipe_name):
         image_file=io.BytesIO(img_content) 
         image=Image.open(image_file)
         file_name=str(img_no)+recipe_name
-        file_path=folder+file_name
+        file_path=os.path.join(folder,file_name)
         with open(file_path,"wb") as f:# wb ---> write bytes
             image.save(f,"JPEG") 
-            print('Success')    
+            print('Successful download ',img_no)    
     except:
         print("Failed")
         
 def download_images(download_path,recipe_name,recipe_index,urls,count):
     try:
-        recipe_name=recipe_name+str(recipe_index)
-        train=os.path.join(download_path,recipe_name,"train")
-        test=os.path.join(download_path,recipe_name,"test")
+        recipe_name1=recipe_name+str(recipe_index)
+        train=os.path.join(download_path,"train",recipe_name1)
+        test=os.path.join(download_path,"test",recipe_name1)
         if not os.path.exists(train):
             os.makedirs(train)
         if not os.path.exists(test):
@@ -76,10 +77,10 @@ try:
             thumbnails = driver.find_elements(By.CLASS_NAME,"mNsIhb") 
         
         image_urls=set()  
-        count=0
+        successful_count=0
         for thumbnail in thumbnails:
             #print(len(image_urls))
-            if(count == maximum_images):
+            if(successful_count == maximum_images):
                 break
             try:
                 thumbnail.click()
@@ -103,15 +104,15 @@ try:
                                 img_url = image.get_attribute("src")
                                 if img_url not in image_urls :
                                     image_urls.add(img_url)
-                                    count=count+1
-                                    print("success")
+                                    successful_count=successful_count+1
+                                    print("success ")
                             else:
                                 continue
             time.sleep(7)
         
         for url in image_urls:
             print(url)
-        download_images("C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\ADITI\\download_images",recipe_name,image_urls,start+i,train_images_count)
+        download_images("C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\ADITI\\download_images",recipe_name,start+i,image_urls,train_images_count)
         time.sleep(7)
             
 except:
