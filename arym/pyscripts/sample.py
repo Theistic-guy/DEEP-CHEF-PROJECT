babf5af0c67c854d292a0cecacb1fd2221d785c2
Theistic-guy <aryamanverma11@gmail.com>
2024-04-24 17:54:50 +0530

    new commits

--- arym/pyscripts/sample.py
+++ arym/pyscripts/sample.py
@@ -3,4 +3,5 @@ path_logs = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\arym\t
 path_driver = r"C:\Users\aryam\Documents\ML\ImageToRecipe\chromedriver.exe"
 path_csv = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\arym\csv\links_copy.csv"
 path_download_folder = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\downloaded_images"
-ru.reload_urls_and_save(path_download_folder,path_logs,path_csv,4,8)
+
+ru.view_recipe(path_logs,5,path_driver,2)
