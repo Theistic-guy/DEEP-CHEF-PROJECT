937ac7d28ce335ce128f605f464140629e65514e
aditi-singh-21 <aditisinghrk906@gmail.com>
2024-04-24 15:28:10 +0530

    new

--- ADITI/webdriver.py
+++ ADITI/webdriver.py
@@ -14,7 +14,6 @@ from utils import download_images,logfile_urls
 from selenium.webdriver.chrome.options import Options
 
 driver=None
-search_box=None
 
 path = "C:\\Users\\aditi\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
 services=webdriver.ChromeService(executable_path=path) #creates an instance of Chrome WebDriver, which allows you to interact  with the Chrome browser.
@@ -23,23 +22,23 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach",True)
 
 def create_driver_load(services,option):
-    global driver,search_box
+    global driver
     try:
         driver = webdriver.Chrome(service=services,options=option) # Create a new Chrome session and pass the service object
         driver.maximize_window()
         driver.get("https://images.google.com/")
         time.sleep(7)
-        search_box=driver.find_element(By.NAME,'q')
-    except:
+    except Exception as e :
         print("Error file creating and loading the driver")
     
 try:
     create_driver_load(services,chrome_options)
-    start=669
+    start=668
     count=2
     maximum_images=10
     train_images_count=8
     extra_images=5
+    new_driver_count=3
     
     df = pd.read_csv('C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\ADITI\\recipes.csv',skiprows= start,nrows=count,names=['name','link'])
     
@@ -56,6 +55,8 @@ try:
         log_file.write("\n")
         log_file.flush()
         
+        search_box = driver.find_element(By.NAME,"q")
+        search_box.clear()
         search_box.send_keys(recipe_name)
         search_box.send_keys(Keys.ENTER)
         time.sleep(10)
@@ -67,7 +68,10 @@ try:
             print("less_images")
             driver.quit()
             print("create new driver")
+            new_driver_count=new_driver_count-1
             create_driver_load(services,chrome_options)
+            if new_driver_count < 0:
+                raise
             i=i-1
         
         image_urls=list()  
@@ -105,21 +109,22 @@ try:
                                         successful_count=successful_count+1
                                         print("success ",successful_count)
                                     else:
+                                        print("inaccessbile")
                                         continue
                                 else:
                                     print("Failed Duplicate")
                                     continue
             time.sleep(7)
-        logfile_urls(image_urls,train_images_count,log_file)
-        download_images("C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\ADITI\\download_images",recipe_name,start+i-1,image_urls,train_images_count)
+        logfile_urls(image_urls,train_images_count,"C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\ADITI\\recipes_logfiles.txt")
+        download_images("C:\\Users\\aditi\\OneDrive\\Desktop\\PROJECTS\\DEEP-CHEF-PROJECT\\downloaded_images",recipe_name,start+i-1,image_urls,train_images_count)
         
         time.sleep(7)
         i=i+1
     log_file.close()
 
 
-except:
-    print("unsuccess")  
+except Exception as e:
+    print("unsuccess",e)  
     driver.quit()    
             
             
