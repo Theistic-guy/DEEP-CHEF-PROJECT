04ba970c531f9b20e05eab964697d40053d78c51
aditi-singh-21 <aditisinghrk906@gmail.com>
2024-04-24 21:44:59 +0530

    new

--- ADITI/webdriver.py
+++ ADITI/webdriver.py
@@ -33,7 +33,7 @@ def create_driver_load(services,option):
     
 try:
     create_driver_load(services,chrome_options)
-    start=683
+    start=685
     count=1
     maximum_images=10
     train_images_count=8
