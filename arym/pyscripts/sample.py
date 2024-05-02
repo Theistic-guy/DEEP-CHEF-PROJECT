ce6888403efc47a3963b99f50c24cc5692f12673
Theistic-guy <aryamanverma11@gmail.com>
2024-05-02 21:29:35 +0530

    new images

--- arym/pyscripts/sample.py
+++ arym/pyscripts/sample.py
@@ -9,7 +9,7 @@ path_download_folder = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PRO
 
 
 # reload
-for i in range(58,59):
+for i in range(63,73):
     ru.reload_urls_and_save(path_download_folder,path_logs,path_csv,i,8)
 
 
