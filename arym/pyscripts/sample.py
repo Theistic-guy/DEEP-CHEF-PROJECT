8ceee3032991de08641cd640c53c483845ab9a8c
Theistic-guy <aryamanverma11@gmail.com>
2024-05-10 21:54:35 +0530

    new commits

--- arym/pyscripts/sample.py
+++ arym/pyscripts/sample.py
@@ -1,19 +1,30 @@
 import recipe_utils as ru
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
 path_logs = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\arym\txt\download_logs.txt"
 path_driver = r"C:\Users\aryam\Documents\ML\ImageToRecipe\chromedriver.exe"
 path_csv = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\links_copy_main.csv"
 path_download_folder = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\downloaded_images"
+path_json = r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\arym\json\recipes.json"
+'''
+Potato Curry with Spinach and Chickpeas  --  Sweet Potato Curry With Spinach and Chickpeas
+Chicken Makhani (Indian Butter Chicken)  --  Chicken Makhani
+Cashew Chicken  --  Cashew Chicken Curry
+Cucumber Salad  --  Cucumber Salad with curd
+Ginger Yogurt  --  Ginger Yogurt recipe
+Indian Fry Bread  --  American Indian Fry Bread
+Garbanzo Beans  --  Garbanzo Beans recipe
+'''
 
 
-
-# reload
-for i in range(113,114):
-    ru.reload_urls_and_save(path_download_folder,path_logs,path_csv,i,8)
-
-
-# print(ru.check_logs_integrity(path_logs,10))
-
-# ru.download_image(r"C:\Users\aryam\Documents\ML\ImageToRecipe\DEEP-CHEF-PROJECT\downloaded_images\train\\38_Aloo Palak","https://manjulaskitchen.com/wp-content/uploads/aloo_palak.jpg"\
-#                   ,"8_Aloo Palak.jpg")
+# with open(path_logs,"r") as f:
+#     df = pd.read_csv(path_csv)
+#     index = 0
+#     for line in f:
+#         if("->" in line):
+#             name_in_log = line.split("->")[1].strip()
+#             name_in_csv = df.at[index,'name'].strip()
+#             if(name_in_log != name_in_csv):
+#                 print(name_in_log," -- ",name_in_csv)
+#             index+=1
